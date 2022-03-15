d95ccac467671f24aa9e160fdacaf21cff729f62
Jordan Lee <jordanl.7890@gmail.com>
2022-03-15 14:19:29 -0700

    clean up organization

--- skgrid/scripts/train.sh
+++ skgrid/scripts/train.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#COPY create_grid.py run_learn.py runner.py run_train.py train.sh  ./
 
 # Build full skgrid model library
-python create_grid.py \
+python scripts/create_grid.py \
 	--config data/src/grid.yml  \
 	--outdir data/output/model_library -n 1
 
 # Train on TMP data and output trained model (pickle)
-python run_train.py --ft_list data/src/featurelist.txt \
+python scripts/run_train.py --ft_list data/src/featurelist.txt \
 	--ft_file data/src/BRCA_v12_20210228.tsv \
 	--model data/output/model_library/RandomForestClassifier.11 \
 	--out data/output/train
