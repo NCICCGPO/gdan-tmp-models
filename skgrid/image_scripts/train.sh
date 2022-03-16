adcb5bb9a929eabc6612ba8cb4b07613af559e7b
Jordan Lee <jordanl.7890@gmail.com>
2022-03-16 16:11:49 -0700

    midway to use platform instead

--- skgrid/image_scripts/train.sh
+++ skgrid/image_scripts/train.sh
@@ -6,6 +6,6 @@ python image_scripts/create_grid.py \
 
 # Train on TMP data and output trained model (pickle)
 python image_scripts/run_train.py --ft_list data/src/featurelist.txt \
-	--ft_file data/src/BRCA_v12_20210228.tsv \
+	--ft_file data/input/BRCA_v12_20210228.tsv \
 	--model data/model_library/RandomForestClassifier.11 \
 	--out data/train
--- skgrid/predict.sh
+++ skgrid/predict.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Predict subtypes
+
 data=${1}
 cancer=${2}
-model=${3}
+# model=${3}
+platform=GEXP
+
+# Determine which model to use
+model=$(python /skgrid/pick_model.py --cancer ${cancer} --platform ${platform})
+
+# Predict subtypes
 python /skgrid/prediction_runner.py \
 	--data ${data} \
 	--cancer  ${cancer} \
-	--model ${model}
+	--model "/skgrid/data/train/${model}"
