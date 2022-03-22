471831d2da32d9fc90ff785a082a4163560aa184
Jordan Lee <jordanl.7890@gmail.com>
2022-03-22 11:29:10 -0700

    clean up train script

--- skgrid/train.sh
+++ skgrid/train.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 cancer=${1}
 platform=${2}
-# echo 'Building model library and training ML models'
-
-# # Build full skgrid model library
-# python image_scripts/create_grid.py \
-# 	--config data/src/grid.yml  \
-# 	--outdir data/model_library -n 1
-
 # Train on TMP data and output trained model in train/ (pickle)
 echo 'Training model'
 python /skgrid/run_train.py \
