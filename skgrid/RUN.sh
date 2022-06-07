a2ba7c936b350d56ac260c20f84870f7b4c775d2
Jordan Lee <jordanl.7890@gmail.com>
2022-06-07 14:36:47 -0700

    cwl out

--- skgrid/RUN.sh
+++ skgrid/RUN.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-mkdir -p data/input
 mkdir -p data/model_library
-mkdir -p data/preds
 mkdir -p data/src
 mkdir -p data/train
 
 # # Run extract model parameters, train model, and predict
 # # cwl-runner --outdir data/preds \
-cwl-runner \
+cwl-runner --outdir preds \
   workflows/FULL_workflow.cwl \
   ../user-job-ymls/skgrid-inputs.yml
 
 # Clean up
-mv Classifier.* data/model_library
-mv *.model data/train
-mv *_preds.tsv preds
+mv preds/Classifier.* data/model_library
+mv preds/*.model data/train
 echo '###'
 echo 'ML model parameters saved in skgrid/data/model_library/'
 echo 'Trained ML model pickle saved in skgrid/data/train/'
