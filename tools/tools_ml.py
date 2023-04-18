033de81fbb96d6740f4603f2e7ce3acbea2ac249
Jordan Lee <jordanl.7890@gmail.com>
2023-04-18 15:29:21 -0700

    Update tools_ml.py

--- tools/tools_ml.py
+++ tools/tools_ml.py
@@ -1,17 +1,29 @@
 def cnvr_converter(data_symbol_list, cancer):
     '''input list of gene symbol (gene name) strings for CNVR. output feature name in TMP nomenclature'''
     import json
-
     # Open conversion file and convert
-    with open('tools/symbol2tmp_{}_CNVR.json'.format(cancer), 'r') as fh:
+    with open('tools/ft_name_convert/symbol2tmp_{}_CNVR.json'.format(cancer), 'r') as fh:
         symbol2tmp = json.load(fh)
-    return [symbol2tmp[e]['TMP_representative_ft'] for e in data_symbol_list]
+
+        results = []
+        for e in data_symbol_list:
+            try:
+                results.append(symbol2tmp[e]['TMP_representative_ft'])
+            except:
+                results.append('nan')
+    return results
+
 
 def gexp_converter(data_entrez_list, cancer):
     '''input list of entrez strings for GEXP. output feature name in TMP nomenclature'''
     import json
-
     # Open conversion file and convert
-    with open('tools/entrez2tmp_{}_GEXP.json'.format(cancer), 'r') as fh:
+    with open('tools/ft_name_convert/entrez2tmp_{}_GEXP.json'.format(cancer), 'r') as fh:
         entrez2tmp = json.load(fh)
-    return [entrez2tmp[e] for e in data_entrez_list]
+        results = []
+        for e in data_entrez_list:
+            try:
+                results.append(entrez2tmp[e])
+            except:
+                results.append('nan')
+    return results
