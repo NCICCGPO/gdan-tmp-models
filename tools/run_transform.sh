c6d60238c1c16fdce5a6c50fc33e9f25c51462a5
Jordan Lee <jordanl.7890@gmail.com>
2023-05-04 11:29:27 -0700

    point to universal tarball

--- tools/run_transform.sh
+++ tools/run_transform.sh
@@ -5,5 +5,5 @@ cancer=${2}
 # Transform data
 python tools/quantile_rescale.py \
 	--src ${user_data} \
-	--dst skgrid/data/src/training_data/${cancer}_v12_20210228.tsv \
+	--dst tools/TMP_20230209/${cancer}_v12_20210228.tsv \
 	--out user-transformed-data/transformed-data.tsv
